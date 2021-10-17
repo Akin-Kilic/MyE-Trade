b40d1b89d7d69050959763cb0f5549bd59ae7534
Akın KILIÇ <akin602000@gmail.com>
2021-10-17 15:18:46 +0300

    Add project files.

---
+++ MyE-Trade/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyE_Trade
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitInStock { get; set; }
+        public string Description { get; set; }
+
+        public Product(int Id, string Name,decimal UnitPrice, int UnitInStock,string Description)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.UnitPrice = UnitPrice;
+            this.UnitInStock = UnitInStock;
+            this.Description = Description;
+        }
+    }
+}
---
+++ MyE-Trade/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyE_Trade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product(1,"Elma",5,15,"Amasya elması");
+            Product product2 = new Product(2,"Armut",6,30,"Taze armut");
+            Product product3 = new Product(3,"İncir",10,100,"Aydın inciri");
+
+            Product[] products1 = new Product[] { product1, product2, product3 };
+            for (int i = 0; i < products1.Length; i++)
+            {
+                Console.WriteLine(products1[i].Name);
+            }
+
+            Console.WriteLine("---------------------------------");
+
+            List<Product> products2 = new List<Product> { product1,product2,product3};
+            foreach (var product in products2)
+            {
+                Console.WriteLine(product.Name);
+            }
+
+            Console.WriteLine("---------------------------------");
+
+            int j = 0;
+            while (j<products1.Length)
+            {
+                Console.WriteLine(products1[j].Name);
+                j++;
+            }
+        }
+    }
+}
